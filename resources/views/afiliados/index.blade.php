1852f246e02d13e9202d0e2b45dd7be43efefe88
Manuel <manuel.carpio@elebegraph.com>
2017-04-24 15:27:06 -0500

    se trabajaron los estilos de la tabla de afiliados, el detalle del afiliado y se comenzó a trabajar en la bolsa de trabajo

--- resources/views/afiliados/index.blade.php
+++ resources/views/afiliados/index.blade.php
@@ -5,28 +5,76 @@
 	        Afiliados
 	    </h1>
 
+	  {{--   <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
+				        <thead>
+				            <th>EMPRESA</th>
+				            <th>CIUDAD</th>
+				            <th>SERVICIOS</th>
+				            <th>CERTIFICACIONES</th>
+				            <th>DETALLES</th>
+				        </thead>
+				        <tbody>
+				            @foreach($afiliados as $afiliado) 
+				            <tr>
+				                <td>{{$afiliado->empresa}}</td>
+				                <td>{{$afiliado->ciudad_id}}</td>
+				                <td>{{$afiliado->servicio_id }}</td>
+				                <td>{{$afiliado->certificados }}</td>
+				                <td><a href="/detalle_afiliado/{{$afiliado->id}}">Ver detalle</a></td>
+				            </tr>
+				            @endforeach 
+				        </tbody>
+				    </table>
+	    {!! $afiliados->render() !!} --}}
+
 	    <br>
-	    <table class = "table table-striped table-bordered table-hover" style = 'background:#fff'>
-	        <thead>
-	            <th>EMPRESA</th>
-	            <th>CIUDAD</th>
-	            <th>SERVICIOS</th>
-	            <th>CERTIFICACIONES</th>
-	            <th>DETALLES</th>
-	        </thead>
-	        <tbody>
-	            @foreach($afiliados as $afiliado) 
-	            <tr>
-	                <td>{{$afiliado->empresa}}</td>
-	                <td>{{$afiliado->ciudad_id}}</td>
-	                <td>{{$afiliado->servicio_id }}</td>
-	                <td>{{$afiliado->certificados }}</td>
-	                <td><a href="/detalle_afiliado/{{$afiliado->id}}">Ver detalle</a></td>
-	            </tr>
-	            @endforeach 
-	        </tbody>
-	    </table>
+	 
+
+	    <div id="empresas">
+	    	<div class="ocho80">
+	    		<div id="selects">
+	    			<form name="form1" method="post" action="">
+	    				<select name="idarea" onchange="obtenerSubArea(this.value)" style="width:190px; height:24px; float:left; margin-right:10px">
+							<option value="0">Seleccione una área</option>
+							<option value="1">A- Economía, Inversión y Finanzas</option>
+							<option value="2">B- Administración y Gestión</option>
+							<option value="3">C- Planeación y Diseño</option>
+							<option value="4">D- Administración de Obras</option>
+							<option value="5">E- Servicios Tecnológicos Profesionales</option>
+						</select>
+							<div id="muestrasubarea" style="float:left; margin-right:10px"></div>
+							<div id="muestraciudad" style="float:left; margin-right:10px"></div>
+							<div id="muestracertsino" style="float:left; margin-right:10px"></div>
+							<div id="muestrasubmit" style="float:left; margin-right:10px"></div>
+	    			</form>
+	    		</div>
+	    		<div id="paginacion"> </div>
+	    		   <table cellpadding="10">
+				        <thead>
+				            <th>EMPRESA</th>
+				            <th>CIUDAD</th>
+				            <th>SERVICIOS</th>
+				            <th>CERTIFICACIONES</th>
+				            <th>DETALLES</th>
+				        </thead>
+				        <tbody>
+				            @foreach($afiliados as $afiliado) 
+				            <tr>
+				                <td>{{$afiliado->empresa}}</td>
+				                <td>{{$afiliado->ciudad_id}}</td>
+				                <td>{{$afiliado->servicio_id }}</td>
+				                <td>{{$afiliado->certificados }}</td>
+				                <td><a href="/detalle_afiliado/{{$afiliado->id}}">Ver detalle</a></td>
+				            </tr>
+				            @endforeach 
+				        </tbody>
+				    </table>
 	    {!! $afiliados->render() !!}
+	    	</div>
+	    </div>
+
+
+
 
 	</section>
 @endsection
--- resources/views/bolsa_trabajo/create.blade.php
+++ resources/views/bolsa_trabajo/create.blade.php
@@ -49,5 +49,73 @@
         <button class = 'btn btn-primary' type ='submit'>Crear</button>
     </form>
 </section>
+
+        <div id="crear_vacante">
+            <div class="ocho80">
+                <div id="selects"></div>
+                <div id="paginacion"> </div>
+                <form enctype="multipart/form-data" method = 'POST' action = '{!!url("bolsa_trabajo")!!}'>
+                    <div class="sub-main">
+                      <button class="button-two" type = 'submit'><span class="texto_blanco">Noticias</span></button>
+                    </div>
+                </form>
+                <br>
+                <table style="margin:45px 0 -25px;" width="882">
+                    <thead>
+                        <tr>
+                            <td>EDITAR CONTACTO</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td bgcolor="#455560">
+                                <form class="forma3" enctype="multipart/form-data" method = 'POST' action = '{!!url("bolsa_trabajo")!!}'>
+                                    <input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
+                                    <div class="form-group">
+                                        <label for="titulo">Titulo Vacante</label>
+                                        <input id="titulo" name = "titulo" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="sueldo">Sueldo</label>
+                                        <input id="sueldo" name= "sueldo" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="empresa">Empresa</label>
+                                        <input id="empresa" name= "empresa" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="imagen">Logo Empresa</label>
+                                        <input id="imagen" name = "imagen" type="file" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="start">Inicio vacante</label>
+                                        <input id="start" name = "start" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="end">Fin vacante</label>
+                                        <input id="end" name = "end" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="color">Activo</label><br>
+                                        <input type="radio" name="activo" value=1> Activo <br>
+                                        <input type="radio" name="activo" value=0> Inactivo <br>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="descripcion">Descripcion</label>
+                                        <textarea  id="descripcion" name = "descripcion" type="text" class="form-control"></textarea>
+                                    </div>
+                                    <div class="sub-main_crear">
+                                      <button class="button-two_crear" type = 'submit'><span class="texto_blanco">Crear</span></button>
+                                    </div>
+                                </form>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+
 @endsection
 @extends('layouts.js')
